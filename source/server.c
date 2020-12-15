5009f046d68e89d4277141e54b18a003de24d6b7
sultanyaril <sultanyaril@gmail.com>
2020-12-15 15:07:06 +0500

    works w/ browser

--- source/server.c
+++ source/server.c
@@ -11,6 +11,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+char HEADER_HTTP[] = "HTTP/1.1 ";
+char HEADER_LENGTH[] = "content-length: ";
+char HEADER_TYPE[] = "content-type: ";
+
+char HTTP_VALUE[2][6] = {
+    "200\r\n",
+    "404\r\n"
+};
+
+char TYPE_VALUE[1][100] = {
+    "text/html\r\n"
+};
+
+char EMPTY_STRING[] = "\r\n";
 
 enum errors {
     OK,
@@ -83,7 +97,7 @@ char *get_path(int client_socket) {
     free(cont);
     
     path = get_word(client_socket);
-
+    
     cont = get_word(client_socket);
     if (strcmp("HTTP/1.1", cont)) {
         perror("incorrect query #2");
@@ -97,7 +111,7 @@ char *get_path(int client_socket) {
     free(cont);
 
     cont = get_word(client_socket);
-    if (strcmp("127.0.0.1", cont)) {
+    if (strcmp("127.0.0.1:5000", cont)) {
         perror("incorrect query #4");
     }
     free(cont);
@@ -107,7 +121,8 @@ char *get_path(int client_socket) {
 
 int request_is_text(char *path) {
     int i = 0;
-    for ( ; path[i] != '\0' && path[i] != '.'; i++);
+    for ( ; path[i] != '\0' && path[i] != '.'; i++) {
+    }
     if (i != strlen(path))
         return 1;
     return 0;
@@ -120,42 +135,61 @@ int size_long(long inp) {
     return answ;
 }
 
-char *telnet_text(char *request_path) {
-    int fd = open(request_path, O_RDONLY, 0);   
+char *append(char *dest, char *src) {
+    dest = realloc(dest, sizeof(char) * (strlen(dest) + strlen(src) - 1));
+    strcat(dest, src);
+    return dest;
+}
+
+char *get_file_size(char *path) {
+    char *answ = NULL;
+    struct stat stats;
+    if (stat(path, &stats) != 0) {
+        perror("stat error");
+    }
+    long size = stats.st_size;
+    int length = 0;
+    for (int size_copy = size; size_copy; length++) {
+        size_copy /= 10;
+    }
+    answ = malloc(length);
+    sprintf(answ, "%ld", size);
+    // answ = append(answ, EMPTY_STRING);
+    return answ;
+}
+
+char *get_file_content(char *path, int fd) {
+    struct stat stats;
+    stat(path, &stats);
+    char *buff = malloc(stats.st_size);
+    read(fd, buff, stats.st_size);
+    return buff;
+}
+
+char *get_response_text(char *request_path) {
+    int fd = open(request_path, O_RDONLY, 0);
+    char *answ = NULL;
     if (fd < 0) {
-        char head[] = "HTTP/1.1 404\n";
-        char *answ = malloc(sizeof(char) * strlen(head));
-        strcpy(answ, head);
+        answ = malloc(strlen(HEADER_HTTP));
+        strcpy(answ, HEADER_HTTP);
+        answ = append(answ, HTTP_VALUE[1]);
+        close(fd);
         return answ;
     }
-    char head[] = "HTTP/1.1 200\ncontent-type: ";
-    char *answ = malloc(sizeof(char) * strlen(head));
-    strcpy(answ, head);
-    int type_ind = 0;
-    for ( ; request_path[type_ind] != '.'; type_ind++);
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + strlen(&request_path[type_ind + 1]) + 1 ));
-    strcat(answ, &(request_path[type_ind + 1]));
-    char cont[] = "/text\ncontent-size: ";
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + strlen(cont) - 1));
-    strcat(answ, cont);
-    struct stat stats;
-    if (stat(request_path, &stats) != 0)
-        perror("stat error");
-    char *str_size = malloc(sizeof(char) * (size_long(stats.st_size) - 1));
-    sprintf(str_size, "%ld", stats.st_size);
-    str_size[size_long(stats.st_size)] = '\0';
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + strlen(str_size) - 1));
-    strcat(answ, str_size);
-    free(str_size);
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + 1));
-    strcat(answ, "\n");
-    char *buff = malloc(sizeof(char) * (stats.st_size + 1));
-    read(fd, buff, stats.st_size);
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + strlen(buff) - 1));
-    strcat(answ, buff);
-    free(buff);
-    answ = realloc(answ, sizeof(char) * (strlen(answ) + 1));
-    strcat(answ, "\n");
+    char *size_string = get_file_size(request_path);
+    char *content = get_file_content(request_path, fd);
+    answ = malloc(strlen(HEADER_HTTP));
+    strcpy(answ, HEADER_HTTP);
+    answ = append(answ, HTTP_VALUE[0]);
+    answ = append(answ, HEADER_TYPE);
+    answ = append(answ, TYPE_VALUE[0]);
+    answ = append(answ, HEADER_LENGTH);
+    answ = append(answ, size_string);
+    answ = append(answ, EMPTY_STRING);
+    answ = append(answ, EMPTY_STRING);
+    answ = append(answ, content);
+    free(size_string);
+    free(content);
     close(fd);
     return answ;
 }
@@ -179,11 +213,9 @@ int telnet_bin(int client_socket, char *request_path) {
     if (pid == 0) {
         dup2(client_socket, 1);
         char *cmd = malloc(sizeof(char) * 3);
-        // strcpy(cmd, "./\0");
-        // strcat(cmd, request_path);
         int res = execl(request_path, request_path, NULL);
         if (res == -1) {
-            char head[] = "HTTP/1.1 404\n";
+            char head[] = "HTTP/1.1 404\r\n";
             write(1, head, strlen(head));
         }
         exit(0);
@@ -193,7 +225,8 @@ int telnet_bin(int client_socket, char *request_path) {
 int interaction_client(int client_socket) {
     char *request_path = get_path(client_socket);
     if (request_is_text(request_path)) {
-        char *response = telnet_text(request_path);
+        char *response = get_response_text(request_path + 1);
+        printf("%s\n", response);
         send_to_client(client_socket, response);
         free(response);
         free(request_path);
@@ -233,7 +266,7 @@ int main(int argc, char** argv) {
                                 (struct sockaddr *) &client_address,
                                 &size);
                 printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
-                                ntohs(client_address.sin_port));    
+                                ntohs(client_address.sin_port));
                 interaction_client(client_socket);
                 close(client_socket);
             }
